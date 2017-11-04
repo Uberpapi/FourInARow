7bdcea77b96a7963e10e547f2eadcdebe8b93644
Alex Lindström <alex.lindstrom92@hotmail.com>
2017-11-04 01:01:55 +0100

    Print fixed and modules divided

--- Game.pl
+++ Game.pl
@@ -1,3 +1,5 @@
+:-module(game, [createBoard/2, hej/0]).
+:-use_module(printboard).
 
 /*Creates a a game Board with the
   size LengthXLength           */
@@ -33,87 +35,3 @@ reverseList(L, R):-
 reverseList([], R, R).
 reverseList([H|T], Acc, R):-
   reverseList(T, [H|Acc], R).
-
-row(M):-
-  tMatrix(M, R),
-  (yRow(M) ; yRow(R); dRow(M)).
-
-/*Checks if we have four in a
-  row Vertical/Horisontal  */
-yRow([[]|Rest]):-
-  yRow(Rest).
-yRow([[H,H,H,H|_]|_]).
-yRow([[_|T]|Rest]):-
-  yRow([T|Rest]).
-
-/*Checks if we have four in a
-  row diagonaly           */
-dRow(M):-
-  dRow(M, 1, 1).
-dRow([[]|Rest], ColNr, _):-
-  NewColNr is ColNr + 1,
-  dRow(Rest, NewColNr, 1).
-dRow(M, ColNr, RowNr):-
-  listLength(M, Count),
-  Count > 3,
-  diagonal(M, RowNr, ColNr), print('Diagonal').
-dRow([[_|T]|Rest], ColNr, RowNr):-
-  NewRowNr is RowNr + 1,
-  dRow([T|Rest], ColNr, NewRowNr).
-
-/*Chooses where to check from depending on
-  which RowNumber we are currently on   */
-diagonal([[H|T]|Rest], RowNr, ColNr):-
-  RowNr < 3,
-  diagonalU([[H|T]|Rest], RowNr, ColNr, 0,  H).
-diagonal([[H|T]|Rest], RowNr, ColNr):-
-  RowNr > 3,
-  diagonalD([[H|T]|Rest], RowNr, ColNr, 0,  H).
-
-/*Checking if we have 4 in a row
-  diagonaly from up to down   */
-diagonalU(_, _, _, 3, _).
-diagonalU([[H|T]|Rest], RowNr, ColNr, Count, Ele):-
-  NewRowNr is RowNr + 1,
-  NewColNr is ColNr + 1,
-  getRow([[H|T]|Rest], NewColNr, Row),
-  getElement(Row, NewRowNr, Ele),
-  NewCount is Count + 1,
-  diagonalU(Rest, NewRowNr, ColNr, NewCount, Ele).
-
-/*Checking if we have 4 in a row
-  diagonaly from down to up   */
-diagonalD(_, _, _, 3, _).
-diagonalD([[H|T]|Rest], RowNr, ColNr, Count, Ele):-
-  NewRowNr is RowNr - 1,
-  NewColNr is ColNr + 1,
-  getRow([[H|T]|Rest], NewColNr, Row),
-  getElement(Row, NewRowNr, Ele),
-  NewCount is Count + 1,
-  diagonalD(Rest, NewRowNr, ColNr, NewCount, Ele).
-
-/*Returns the nth Row */
-getRow([Row|_], 1, Row).
-getRow([_|Rest], Count, Row):-
-  NewCount is Count - 1,
-  getRow(Rest, NewCount, Row).
-
-/*Returns the nth Element */
-getElement([Element|_], 1, Element).
-getElement([_|Rest], Count, Element):-
-  NewCount is Count - 1,
-  getElement(Rest, NewCount, Element).
-
-/*Returns a Matrix transposed*/
-tMatrix([[]|_], []).
-tMatrix(M, [L|R]):-
-  getFirst(M, Rest, L), tMatrix(Rest, R).
-
-getFirst([], [], []).
-getFirst([[H|T]|Rest], [T|Q], [H|Res]):-
-  getFirst(Rest, Q, Res).
-
-%Returns the length of a list
-listLength([], 0).
-listLength([_|Rest], NewLength):-
-  listLength(Rest, Length), NewLength is Length + 1.
---
+++ printboard.pl
@@ -0,0 +1,44 @@
+:-module(printboard, [print/0]).
+:-use_module(game).
+:-use_module(rules).
+
+/* Prints our current board */
+print:-
+  createBoard(6, Q), transformValue(Q, A),
+  getRow(A, 1, B), getRow(A, 2, C), getRow(A, 3, D), getRow(A, 4, E), getRow(A, 5, F), getRow(A, 6, G),
+  getElement(B, 1, A1), getElement(B, 2, A2), getElement(B, 3, A3), getElement(B, 4, A4), getElement(B, 5, A5), getElement(B, 6, A6),
+  getElement(C, 1, B1), getElement(C, 2, B2), getElement(C, 3, B3), getElement(C, 4, B4), getElement(C, 5, B5), getElement(C, 6, B6),
+  getElement(D, 1, C1), getElement(D, 2, C2), getElement(D, 3, C3), getElement(D, 4, C4), getElement(D, 5, C5), getElement(D, 6, C6),
+  getElement(E, 1, D1), getElement(E, 2, D2), getElement(E, 3, D3), getElement(E, 4, D4), getElement(E, 5, D5), getElement(E, 6, D6),
+  getElement(F, 1, E1), getElement(F, 2, E2), getElement(F, 3, E3), getElement(F, 4, E4), getElement(F, 5, E5), getElement(F, 6, E6),
+  getElement(G, 1, F1), getElement(G, 2, F2), getElement(G, 3, F3), getElement(G, 4, F4), getElement(G, 5, F5), getElement(G, 6, F6),
+
+  format('        ~`-t~43|~n', []),
+  format('     1 |  ~t~w~10|~t~w~t~2+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~|~43||~n',[A1,'  |  ', B1,'  |  ',C1,'  |  ',D1, '  |  ',E1,'  |  ',F1]),
+  format('       |~t~w~10|~t|~44|~n', ['-----------------------------------'] ),
+  format('     2 |  ~t~w~10|~t~w~t~2+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~|~43||~n',[A2,'  |  ', B2,'  |  ',C2,'  |  ',D2, '  |  ',E2,'  |  ',F2]),
+  format('       |~t~w~10|~t|~44|~n', ['-----------------------------------'] ),
+  format('     3 |  ~t~w~10|~t~w~t~2+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~|~43||~n',[A3,'  |  ', B3,'  |  ',C3,'  |  ',D3, '  |  ',E3,'  |  ',F3]),
+  format('       |~t~w~10|~t|~44|~n', ['-----------------------------------'] ),
+  format('     4 |  ~t~w~10|~t~w~t~2+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~|~43||~n',[A4,'  |  ', B4,'  |  ',C4,'  |  ',D4, '  |  ',E4,'  |  ',F4]),
+  format('       |~t~w~10|~t|~44|~n', ['-----------------------------------'] ),
+  format('     5 |  ~t~w~10|~t~w~t~2+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~|~43||~n',[A5,'  |  ', B5,'  |  ',C5,'  |  ',D5, '  |  ',E5,'  |  ',F5]),
+  format('       |~t~w~10|~t|~44|~n', ['-----------------------------------'] ),
+  format('     6 |  ~t~w~10|~t~w~t~2+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~t~1+~w~|~43||~n',[A6,'  |  ', B6,'  |  ',C6,'  |  ',D6, '  |  ',E6,'  |  ',F6]),
+  format('        ~`-t~43|~n', []),
+  print('          A     B     C     D     E     F    ').
+
+
+/* Transforms all non x/o to another
+   character of our choosing      */
+transformValue([], []).
+transformValue([H|T], [Res|R]):-
+  transformValue(T, R), assign(H, Res).
+
+assign([], []).
+assign([o|T], [o|Q]):-
+  !, assign(T, Q).
+assign([x|T], [x|Q]):-
+  !, assign(T, Q).
+assign([_|T], ['*'|Q]):-
+  assign(T, Q).
